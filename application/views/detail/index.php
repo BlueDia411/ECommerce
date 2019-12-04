0838f89561eb1b1ff16dcde1475f95ac6b26d874
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-04 19:10:34 +0700

    commit pertama

---
+++ application/views/detail/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div>
+        <div class="align-items-start">
+            <img class="size" src="assets/images/Karambit-Gamma-Doppler.jpg" alt="">
+        </div>
+        <div class="col-lg">
+            <h4>
+                Karambit Gamma Doppler
+            </h4>
+        </div>
+    </div>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/slick.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/slick-theme.css">
+    <title><?= $judul; ?></title>
+</head>
+
+<body>
+    <nav class="sticky-top navbar navbar-expand-lg navbar-dark bg-dark transparant">
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url();?>">JAJANS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link mr-3" href="<?= base_url();?>">HOME</a>
+                    <a class="nav-item nav-link mr-3" href="#">SHOP</a>
+                    <a class="nav-item nav-link mr-3" href="#">ABOUT</a>
+                    <a class="nav-item nav-link mr-3" href="#">CONTACT</a>
+                </div>
+            </div>
+            <div class="">
+                <a class="" href="#">
+                    <img src="<?= base_url(); ?>assets/images/shopping-cart.png" width="30" height="30" alt="">
+                </a>
+            </div>
+        </div>
+    </nav>
